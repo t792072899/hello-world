2529499888aaeb780184f5f091717f8f29b98391
wenqiang <wenqiang@wenqiang-PC>
2019-02-22 11:45:59 +0800

    2019-2-22 hello world

--- hello-world/src/Helloworld.java
+++ hello-world/src/Helloworld.java
@@ -3,7 +3,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Helloworld");
+		System.out.println("Hello world");
 
 	}
 
